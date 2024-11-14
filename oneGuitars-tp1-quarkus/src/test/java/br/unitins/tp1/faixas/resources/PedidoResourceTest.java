d08cac8c2473a31538a359ad7d412147a1505fb2
Filipe Ferreira <filipeferreira.psid2019@gmail.com>
2024-11-14 17:23:46 -0300

    ajustando banco de dados de testes

--- oneGuitars-tp1-quarkus/src/test/java/br/unitins/tp1/faixas/resources/PedidoResourceTest.java
+++ oneGuitars-tp1-quarkus/src/test/java/br/unitins/tp1/faixas/resources/PedidoResourceTest.java
@@ -2,36 +2,35 @@ package br.unitins.tp1.faixas.resources;
 
 import static io.restassured.RestAssured.given;
 
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-
 import org.junit.jupiter.api.Test;
 
+import br.unitins.tp1.faixas.Endereco.service.EnderecoService;
 import br.unitins.tp1.faixas.Pedido.service.PedidoService;
 import io.quarkus.test.junit.QuarkusTest;
 import jakarta.inject.Inject;
 
 @QuarkusTest
 public class PedidoResourceTest {
-    
+
     @Inject
     PedidoService pedidoService;
+    @Inject
+    EnderecoService enderecoService;
+    
 
     @Test
-    void testFindById(){
+    void testFindById() {
         given()
                 .when().get("/pedidos/" + 1)
                 .then().statusCode(200);
 
     }
 
-
     @Test
-    void testFindByDataCompra(){
-        LocalDateTime dateTime = LocalDateTime.of(2024, 10, 16, 14, 48, 46);
-        String formattedDateTime = dateTime.format(DateTimeFormatter.ISO_LOCAL_DATE_TIME);
+    void testFindByUsername() {
         given()
-            .when().get("/pedidos/search/"+ formattedDateTime)
-            .then().statusCode(200);
+                .when().get("/marcas/search/" + "carlinhos")
+                .then().statusCode(200);
     }
+
 }
